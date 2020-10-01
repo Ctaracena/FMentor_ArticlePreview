306c7ee43f84803f29515a5c9f7717a0e255ac89
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-10-01 10:00:03 -0500

    console.log out

--- js/main.js
+++ js/main.js
@@ -4,20 +4,19 @@ const shareContainer = document.querySelector('.share-container')
 
 let show = false
 
-console.log(show)
 shareIcon.addEventListener('click', () => {
   if (show == false) {
     shareContainer.style.display = 'flex'
     shareIcon.style.backgroundColor = 'hsl(214, 17%, 51%)'
     icon.src = '../images/white-icon-share.svg'
     show = true
-    console.log(show)
+  
   } else {
     shareContainer.style.display = 'none'
     shareIcon.style.backgroundColor = 'hsl(210, 46%, 95%)'
     icon.src = '../images/icon-share.svg'
     show = false
-    console.log(show)
+  
   }
 } )
 
@@ -35,7 +34,6 @@ const iconMobile = document.querySelector('.share-icon-mobile')
 const shareContainerMobile = document.querySelector('.share-container-mobile')
 const closeShareContainerMobile = document.querySelector('.close-share-container-mobile')
 
-console.log(show)
 shareIconMobile.addEventListener('click', () => {
     shareContainerMobile.style.display = 'flex'
     shareIconMobile.style.backgroundColor = 'hsl(214, 17%, 51%)'
